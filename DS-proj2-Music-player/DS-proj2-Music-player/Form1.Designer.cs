18fd9c606783636de3f6708bfb6c1f1681d03983
Kianoosh <kia.comp.eng@outlook.com>
2022-10-23 00:14:58 +0330

    first menu ui design is complited

--- DS-proj2-Music-player/DS-proj2-Music-player/Form1.Designer.cs
+++ DS-proj2-Music-player/DS-proj2-Music-player/Form1.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace DS_proj2_Music_player
+namespace DS_proj2_Music_player
 {
     partial class MainForm
     {
@@ -28,23 +28,228 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.b4 = new System.Windows.Forms.Panel();
+            this.merge_butt = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Panel();
+            this.b2 = new System.Windows.Forms.Panel();
+            this.b1 = new System.Windows.Forms.Panel();
+            this.plylst_butt = new System.Windows.Forms.Button();
+            this.liked_butt = new System.Windows.Forms.Button();
+            this.add_butt = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(71)))), ((int)(((byte)(86)))));
+            this.panel1.Controls.Add(this.b4);
+            this.panel1.Controls.Add(this.merge_butt);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Controls.Add(this.plylst_butt);
+            this.panel1.Controls.Add(this.liked_butt);
+            this.panel1.Controls.Add(this.add_butt);
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 567);
+            this.panel1.TabIndex = 0;
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(41)))));
+            this.b4.Location = new System.Drawing.Point(0, 476);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(10, 113);
+            this.b4.TabIndex = 9;
+            this.b4.Visible = false;
+            // 
+            // merge_butt
+            // 
+            this.merge_butt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.merge_butt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.merge_butt.FlatAppearance.BorderSize = 0;
+            this.merge_butt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.merge_butt.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.merge_butt.ForeColor = System.Drawing.Color.White;
+            this.merge_butt.Image = ((System.Drawing.Image)(resources.GetObject("merge_butt.Image")));
+            this.merge_butt.Location = new System.Drawing.Point(0, 473);
+            this.merge_butt.Name = "merge_butt";
+            this.merge_butt.Size = new System.Drawing.Size(194, 105);
+            this.merge_butt.TabIndex = 10;
+            this.merge_butt.Text = "Merge Play List";
+            this.merge_butt.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.merge_butt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.merge_butt.UseVisualStyleBackColor = true;
+            this.merge_butt.Click += new System.EventHandler(this.merge_butt_Click);
+            this.merge_butt.MouseEnter += new System.EventHandler(this.merge_butt_MouseEnter);
+            this.merge_butt.MouseLeave += new System.EventHandler(this.merge_butt_MouseLeave);
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(41)))));
+            this.b3.Location = new System.Drawing.Point(0, 381);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(10, 113);
+            this.b3.TabIndex = 9;
+            this.b3.Visible = false;
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(41)))));
+            this.b2.Location = new System.Drawing.Point(0, 251);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(10, 113);
+            this.b2.TabIndex = 8;
+            this.b2.Visible = false;
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(41)))));
+            this.b1.Location = new System.Drawing.Point(0, 132);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(10, 113);
+            this.b1.TabIndex = 7;
+            this.b1.Visible = false;
+            // 
+            // plylst_butt
+            // 
+            this.plylst_butt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.plylst_butt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.plylst_butt.FlatAppearance.BorderSize = 0;
+            this.plylst_butt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.plylst_butt.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.plylst_butt.ForeColor = System.Drawing.Color.White;
+            this.plylst_butt.Image = ((System.Drawing.Image)(resources.GetObject("plylst_butt.Image")));
+            this.plylst_butt.Location = new System.Drawing.Point(0, 132);
+            this.plylst_butt.Name = "plylst_butt";
+            this.plylst_butt.Size = new System.Drawing.Size(194, 105);
+            this.plylst_butt.TabIndex = 6;
+            this.plylst_butt.Text = "Play Lists";
+            this.plylst_butt.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.plylst_butt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.plylst_butt.UseVisualStyleBackColor = true;
+            this.plylst_butt.Click += new System.EventHandler(this.plylst_butt_Click);
+            this.plylst_butt.MouseEnter += new System.EventHandler(this.plylst_butt_MouseEnter);
+            this.plylst_butt.MouseLeave += new System.EventHandler(this.plylst_butt_MouseLeave);
+            // 
+            // liked_butt
+            // 
+            this.liked_butt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.liked_butt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.liked_butt.FlatAppearance.BorderSize = 0;
+            this.liked_butt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.liked_butt.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.liked_butt.ForeColor = System.Drawing.Color.White;
+            this.liked_butt.Image = ((System.Drawing.Image)(resources.GetObject("liked_butt.Image")));
+            this.liked_butt.Location = new System.Drawing.Point(0, 351);
+            this.liked_butt.Name = "liked_butt";
+            this.liked_butt.Size = new System.Drawing.Size(194, 105);
+            this.liked_butt.TabIndex = 4;
+            this.liked_butt.Text = "Liked Songs";
+            this.liked_butt.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.liked_butt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.liked_butt.UseVisualStyleBackColor = true;
+            this.liked_butt.Click += new System.EventHandler(this.liked_butt_Click);
+            this.liked_butt.MouseEnter += new System.EventHandler(this.liked_butt_MouseEnter);
+            this.liked_butt.MouseLeave += new System.EventHandler(this.liked_butt_MouseLeave);
+            // 
+            // add_butt
+            // 
+            this.add_butt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.add_butt.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.add_butt.FlatAppearance.BorderSize = 0;
+            this.add_butt.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.add_butt.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add_butt.ForeColor = System.Drawing.Color.White;
+            this.add_butt.Image = ((System.Drawing.Image)(resources.GetObject("add_butt.Image")));
+            this.add_butt.Location = new System.Drawing.Point(0, 238);
+            this.add_butt.Name = "add_butt";
+            this.add_butt.Size = new System.Drawing.Size(194, 105);
+            this.add_butt.TabIndex = 3;
+            this.add_butt.Text = "Add Play List";
+            this.add_butt.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.add_butt.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.add_butt.UseVisualStyleBackColor = true;
+            this.add_butt.Click += new System.EventHandler(this.add_butt_Click);
+            this.add_butt.MouseEnter += new System.EventHandler(this.add_butt_MouseEnter);
+            this.add_butt.MouseLeave += new System.EventHandler(this.add_butt_MouseLeave);
+            // 
+            // panel3
+            // 
+            this.panel3.AutoScroll = true;
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(76)))), ((int)(((byte)(41)))));
+            this.panel3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Cursor = System.Windows.Forms.Cursors.Help;
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 108);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(200, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(71)))), ((int)(((byte)(86)))));
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(200, 439);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(650, 128);
+            this.panel2.TabIndex = 1;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(57)))), ((int)(((byte)(75)))));
+            this.ClientSize = new System.Drawing.Size(850, 567);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Music Player";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
-        #endregion
-    }
+    #endregion
+
+    private System.Windows.Forms.Panel panel1;
+    private System.Windows.Forms.Panel panel3;
+    private System.Windows.Forms.Panel panel2;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.Button add_butt;
+    private System.Windows.Forms.Button liked_butt;
+    private System.Windows.Forms.Button plylst_butt;
+    private System.Windows.Forms.Panel b3;
+    private System.Windows.Forms.Panel b2;
+    private System.Windows.Forms.Panel b1;
+    private System.Windows.Forms.Panel b4;
+    private System.Windows.Forms.Button merge_butt;
+  }
 }
 
--- DS-proj2-Music-player/DS-proj2-Music-player/Form1.cs
+++ DS-proj2-Music-player/DS-proj2-Music-player/Form1.cs
@@ -10,20 +10,155 @@ using System.Windows.Forms;
 
 namespace DS_proj2_Music_player
 {
+
+  
     public partial class MainForm : Form
     {
-        public MainForm()
+    int b = 0;
+
+    void b_click(int tmp)
+    {
+      switch(tmp)
+      {
+        case 1:
+          b1.Show();
+          b2.Hide();
+          b3.Hide();
+          b4.Hide();
+          b = 1;
+          break;
+        case 2:
+          b1.Hide();
+          b2.Show();
+          b3.Hide();
+          b4.Hide();
+          b = 2;
+          break;
+        case 3:
+          b1.Hide();
+          b2.Hide();
+          b3.Show();
+          b4.Hide();
+          b = 3;
+          break;
+        case 4:
+          b1.Hide();
+          b2.Hide();
+          b3.Hide();
+          b4.Show();
+          b = 4;
+          break;
+        default:
+          b = 0;
+          break;
+      }
+    }
+
+    public MainForm()
         {
             InitializeComponent();
       this.ControlBox = false;
       this.FormBorderStyle = FormBorderStyle.None;
-      
+      add_butt.Top = plylst_butt.Bottom;
+      liked_butt.Top = add_butt.Bottom;
+      merge_butt.Top = liked_butt.Bottom; // to set the order!
+
+      b1.Height = plylst_butt.Height;
+      b2.Height = add_butt.Height;
+      b3.Height = liked_butt.Height;
+      b4.Height = merge_butt.Height;
+
+      b1.Top = plylst_butt.Top;
+      b2.Top = add_butt.Top;
+      b3.Top = liked_butt.Top;
+      b4.Top = merge_butt.Top;
+
+
+
+      //X_butt.Cursor = Cursors.Hand;
+      // size is 850,550
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
       SplashForm f = new SplashForm();
       f.ShowDialog();
+
+      b_click(1);
     }
+
+    #region menu_butts
+
+    private void plylst_butt_Click(object sender, EventArgs e)
+    {
+      b_click(1);
     }
+
+    private void add_butt_Click(object sender, EventArgs e)
+    {
+      b_click(2);
+    }
+
+    private void liked_butt_Click(object sender, EventArgs e)
+    {
+      b_click(3);
+    }
+
+
+    private void merge_butt_Click(object sender, EventArgs e)
+    {
+      b_click(4);
+    }
+
+
+    #endregion
+
+    #region menu_left_border
+
+    private void plylst_butt_MouseLeave(object sender, EventArgs e)
+    {
+      if(b != 1)
+        b1.Hide();
+    }
+
+    private void plylst_butt_MouseEnter(object sender, EventArgs e)
+    {
+      b1.Show();
+    }
+
+    private void add_butt_MouseEnter(object sender, EventArgs e)
+    {
+      b2.Show();
+    }
+
+    private void add_butt_MouseLeave(object sender, EventArgs e)
+    {
+      if (b != 2)
+        b2.Hide();
+    }
+
+
+    private void liked_butt_MouseEnter(object sender, EventArgs e)
+    {
+      b3.Show();
+    }
+
+    private void liked_butt_MouseLeave(object sender, EventArgs e)
+    {
+      if (b != 3)
+        b3.Hide();
+    }
+
+    private void merge_butt_MouseEnter(object sender, EventArgs e)
+    {
+      b4.Show();
+    }
+
+    private void merge_butt_MouseLeave(object sender, EventArgs e)
+    {
+      if (b != 4)
+        b4.Hide();
+    }
+    #endregion
+  }
 }
