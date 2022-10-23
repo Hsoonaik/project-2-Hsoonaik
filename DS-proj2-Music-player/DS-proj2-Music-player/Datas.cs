4f6cb380fb8aba4d890a7e834985541d0b1c4e64
Kianoosh <kia.comp.eng@outlook.com>
2022-10-23 11:15:52 +0330

    Datas and Play List was added!

---
+++ DS-proj2-Music-player/DS-proj2-Music-player/Datas.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_proj2_Music_player
+{
+  internal class Datas
+  {
+    List<PlayList> PList = new List<PlayList>();
+
+
+  }
+}
---
+++ DS-proj2-Music-player/DS-proj2-Music-player/PlayList.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_proj2_Music_player
+{
+  internal class PlayList
+  {
+    public int id { get; set; }
+    public string Name { get; set; }
+    public bool isChecked { get; set; }
+
+    void add_play_list(PlayList P)
+    {
+
+    }
+  }
+}
